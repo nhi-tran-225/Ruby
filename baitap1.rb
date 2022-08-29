8052a4bd0ae2b97f07b56ce301a1da41e72eab24
Phuong Nhi <tranphuongnhi321@gmail.com>
2022-08-29 21:35:03 +0700

    Complete ex1

---
+++ baitap1.rb
@@ -0,0 +1,14 @@
+puts "Hello world!"
+puts "Xin chào Trần Thị Phương Nhi 1911505310140"
+print "Nhập vào 1 số: "
+number = gets
+puts "Số đã nhập là #{number}"
+print "Nhập vào số thứ nhất: "
+number1 = gets
+print "Nhập vào số thứ hai: "
+number2 = gets
+if(number1>number2)
+    puts "Số lớn nhất là #{number1}"
+else
+    puts "Số lớn nhất là #{number2}"
+end
