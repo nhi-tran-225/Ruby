8354b9ca746c1ce085835461d89607e8f6992c0f
Phuong Nhi <tranphuongnhi321@gmail.com>
2022-09-16 13:41:09 +0700

    Baitaptaomang-1609

---
+++ baitaptaoclass-1609/sinhvien.rb
@@ -0,0 +1,26 @@
+class SinhVien
+    def initialize(msv, ten)
+       @sv_msv = msv
+       @sv_ten = ten
+    end
+    def inThongTin()
+       puts "Mã sinh viên #@sv_msv"
+       puts "Tên sinh viên #@sv_ten"
+    end
+    def nhapDiem
+        print "Nhập điểm toán: "
+        toan = gets
+        print "Nhập điểm lý: "
+        ly = gets
+        print "Nhập điểm hoá: "
+        hoa = gets
+        puts 'Điểm trung bình là %.2f' % [(toan.to_f+ly.to_f+hoa.to_f)/3]
+    end
+ end
+ 
+ # Create Objects
+ sv1 = SinhVien.new("1", "Nhi")
+ 
+ # Call Methods
+ sv1.inThongTin()
+ sv1.nhapDiem
